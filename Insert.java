bdd220afdcba98d84ded60acd6d797db004758a6
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-07-29 19:20:35 +0530

    Add files via upload

---
+++ Insert.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+public class Insert {
+	public static void main(String[] args) throws Exception{
+		String url="jdbc:mysql://localhost:3306/mydata", uname="root",pass="kamalika01";
+		int studentId=3;
+		String studentName="Kamalika";
+		String query="insert into students values("+ studentId+",'" +studentName+ "')";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection(url,uname,pass);
+		Statement st=con.createStatement();
+		int c=st.executeUpdate(query);
+		System.out.println(c+" row/s affected");
+		st.close();
+		con.close();
+	}
+}
