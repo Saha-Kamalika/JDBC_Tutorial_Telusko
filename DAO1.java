39ec971b59cd596c07e14d8bcffea756ceec3932
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-07-30 10:52:42 +0530

    Add files via upload

---
+++ DAO1.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+public class DAO1 {
+	public static void main(String[] args) {
+		DetailsDao1 dao=new DetailsDao1();
+		Details1 d=new Details1();
+		d.id=4;
+		d.name="bishtu";
+		dao.addDetails(d);
+	}
+}
+class DetailsDao1{
+	public void addDetails(Details1 d) {
+		try {
+			String url="jdbc:mysql://localhost:3306/mydata", uname="root", pass="kamalika01";
+			String query="insert into details values (?,?)";
+	
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection(url,uname,pass);
+			PreparedStatement st=con.prepareStatement(query);
+			st.setInt(1, d.id);
+			st.setString(2, d.name);
+			st.executeUpdate();
+			st.close();
+			con.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+class Details1{
+	int id;
+	String name;
+}
